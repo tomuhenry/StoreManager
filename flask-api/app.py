2e8ffd48e62ed969433c60c52732768f297947a1
Tomusange Henry <tomusangehenry02@gmail.com>
2018-10-16 18:08:22 +0300

    New changes to the ignore file and endpoint[Delivers #161242645]

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,24 +1,29 @@
-from flask import Flask, jsonify, json, request, abort
+from flask import Flask, jsonify, json, request, abort, session
 from data_models import products, sales
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
 time = str(datetime.now())
 
-@app.errorhandler(404)             
+
+@app.errorhandler(404)
 def not_found(error):
     return jsonify({'error': 'Not found'}), 404
 
-@app.errorhandler(400)             
+
+@app.errorhandler(400)
 def bad_request(error):
     return jsonify({'error': 'Invalid request/input'}), 400
 
+
 @app.route('/store-manager/api/v1/')
 def home():
-   return jsonify({"Welcome":"Welcome to the Store manager api"})
+    return jsonify({"Welcome": "Welcome to the Store manager api"})
 
-@app.route('/store-manager/api/v1/products', methods=['POST'])
+
+@app.route('/store-manager/api/v1/admin/products', methods=['POST'])
 def add_product():
     data = request.json
     product_id = products[-1]['product_id']+1,
@@ -30,7 +35,7 @@ def add_product():
 
     if type(product_stock) != int and type(product_price) != int:
         abort(400)
-    
+
     elif not data or data == "":
         abort(400)
 
@@ -43,8 +48,9 @@ def add_product():
             "product_stock": product_stock,
             "product_price": product_price,
         }
-        products.append(product);
-        return jsonify({"Success":"The product {0} has been added".format(product["product_name"])}), 200
+        products.append(product)
+        return jsonify({"Success": "The product '{0}' has been added".format(product["product_name"])}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
--- flask-api/data_models.py
+++ flask-api/data_models.py
@@ -4,7 +4,7 @@ products = [{
     "product_name": "wine",
     "product_specs": "700ml",
     "product_stock": 15,
-    "product_price": 15000,
+    "product_price": 15000
 }]
 
 sales = [{
@@ -12,5 +12,5 @@ sales = [{
     "product_id": 1,
     "sale_quantity": 3,
     "sale_price": 45000,
-    "date_sold": "2018-10-16 11:40:34",
+    "date_sold": "2018-10-16 11:40:34"
 }]
