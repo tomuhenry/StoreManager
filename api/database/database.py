abee7250fa353ccec9fa0265adc4989af7a4fd5b
tomuhenry <40206038+tomuhenry@users.noreply.github.com>
2018-11-07 10:33:38 +0300

    Update database.py
    Add database info for heroku

--- api/database/database.py
+++ api/database/database.py
@@ -7,7 +7,9 @@ class Database:
 
     def __init__(self):
 
-        self.db_parameters = """dbname='storemanagerdb' user='postgres' password='challenge3'"""
+        self.db_parameters = """dbname='d4eo92qumfels6' user='rydoowkieaxjhf' 
+                    host='ec2-54-83-38-174.compute-1.amazonaws.com' 
+                    password='451025a5501925f1a9c2dad02c65fdd1122b1cc2cfa8d94d021d86e059f74b51'"""
 
         self.admin_pass = generate_password_hash('adminpass')
         self.user_pass = generate_password_hash('userpass')
@@ -103,7 +105,9 @@ class Database:
 
     @staticmethod
     def drop_table(command):
-        db_parameters = """dbname='storemanagerdb' user='postgres' password='challenge3'"""
+        db_parameters = """dbname='d4eo92qumfels6' user='rydoowkieaxjhf' 
+                    host='ec2-54-83-38-174.compute-1.amazonaws.com' 
+                    password='451025a5501925f1a9c2dad02c65fdd1122b1cc2cfa8d94d021d86e059f74b51'"""
         conn = psycopg2.connect(db_parameters)
         curs = conn.cursor()
         curs.execute(command)
